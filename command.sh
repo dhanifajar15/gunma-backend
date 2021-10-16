24426dabc7da7578614f071105ce2b570893223d
Dhani Fajar Nugroho <dhanifajar15@gmail.com>
2021-10-16 10:19:24 +0700

    update command

--- command.sh
+++ command.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-#docker exec -ti gunma-backend_db_1 sh -c "mysql -u root -psecret -e "ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'secret'""
-docker exec -ti gunma-backend_php_1 sh -c "php artisan migrate:fresh"
-docker exec -ti gunma-backend_php_1 sh -c "chmod 777 -R storage"
+#docker exec -ti gunma-backend_db_1 sh -c "mysql -u root -pgunma -e "ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'gunma'""
+#docker exec -ti gunma-backend_php_1 sh -c "php artisan migrate:fresh"
+#docker exec -ti gunma-backend_php_1 sh -c "chmod 777 -R storage"
